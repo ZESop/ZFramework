fbcc6d2da088bbe9b9b99798ce97b2661a174d4e
ZESop <2586075826qq.com>
2019-06-05 15:24:09 +0800

    #0000 update

---
+++ Zframework/Assets/ZFramework/2.复制文本到剪切板/CopyText2Clipboard.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace ZFramework
+{
+    public class CopyText2Clipboard : MonoBehaviour
+    {
+        #if UNITY_EDITOR
+            [MenuItem("ZFramework/0.复制文本到剪切板")]
+        #endif
+        private static void MenuClicked()
+        {
+            GUIUtility.systemCopyBuffer = "复制的文本";
+        }
+    }
+}
