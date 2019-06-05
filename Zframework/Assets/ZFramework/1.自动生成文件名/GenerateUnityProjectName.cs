ca2f29d298274973f8fce4ff7a7570febef8e252
ZESop <2586075826qq.com>
2019-06-05 16:18:07 +0800

    Revert "Revert "#0000 update""
    This reverts commit d71b7cc87f6b4bc8f9c58f2361c3c180c124a850.

---
+++ Zframework/Assets/ZFramework/1.自动生成文件名/GenerateUnityProjectName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace ZFramework
+{
+    public class GenerateUnityProjectName
+    {
+        [MenuItem("ZFramework/1.生成 UnityPackage 名字")]
+        private static void MenuClicked()
+        {
+            Debug.Log("ZFramework_"+DateTime.Now.ToString("yyyyMMdd_HH"));
+        }
+    }
+}
+
